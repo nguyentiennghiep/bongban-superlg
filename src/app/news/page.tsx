04931c8d14001d86e72adf59c0b4d5abf346045e
nghiepnguyentien <nghiepnguyentien@gmail.com>
2025-05-03 17:43:25 +0700

    fix : import err

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Post, postApi } from "@/services/api";
+import { Post, postApi } from "@/services";
 
 export default function NewsPage() {
   const [posts, setPosts] = useState<Post[]>([]);
